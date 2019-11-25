2e2deea890145a61619828bc815a58cff04ed003
guilherme-adesouza <guilherme.souza2@universo.univates.br>
2019-11-24 21:11:02 -0300

    Início implementação e-mail

--- services/activity/register/src/activityRegisterController.js
+++ services/activity/register/src/activityRegisterController.js
@@ -1,20 +1,47 @@
 const ActivityRegisterService = require('./activityRegisterService');
-const request = require('./utils/request');
+const utilsRequest = require('./utils/request');
+const request = require('request');
 
 class ActivityRegisterController {
     constructor() {
         this.activityRegisterService = new ActivityRegisterService();
+        this.mailURL = 'http://localhost:5002/api';
+        this.userURL = 'http://localhost:5002/api';
+    }
+
+    sendSubscriptionEmail(userId){
+        request({
+                url: this.userURL + `/${userId}`,
+                method: 'GET'
+            },
+            function(error, response, body) {
+                const fullUser = JSON.parse(response.body);
+                request({
+                        url: this.mailURL,
+                        method: 'POST',
+                        body: {
+                            email: fullUser.email,
+                            type: "SUBSCRIPTION"
+                        },
+                        json: true
+                    },
+                    function (error, response, body) {
+                        console.log("MAIL REGISTER: ", response);
+                    });
+            }
+        );
     }
 
     create(req, res) {
         const {user, event} = req.body;
-        request(() => {
+        utilsRequest(() => {
             if(!!user && !!event) {
                 this.activityRegisterService.create({
                     id_client: user,
                     id_event: event,
                     status: 'A'
                 }, (props) => {
+                    this.sendSubscriptionEmail(user);
                     res.status(201).send({message: `Activity has been created!`});
                 });
             } else {
